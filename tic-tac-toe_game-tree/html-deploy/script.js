d54c20d353ce9dae0b760dcdac98d95198ab84e4
Polokego <pomakga022@student.wethinkcode.co.za>
2023-12-27 22:06:48 +0200

    Tic-Tac-Toe | added GameState + GameTree

---
+++ tic-tac-toe_game-tree/html-deploy/script.js
@@ -0,0 +1,25 @@
+let block = []
+
+var gameState = {
+
+    prevState: null,
+    stateData: "",
+    lblData: null,
+    nextState: null
+
+}
+let gameTree = ["","","","","","","","","",""]
+
+//list of all winning game states
+const winStates = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8), (0,4,8),(2,4,6)]
+
+//functions for the buttons
+for (let k=0; k < 9; k++) {
+
+    block.append(()=>{
+
+
+
+
+    });
+}
