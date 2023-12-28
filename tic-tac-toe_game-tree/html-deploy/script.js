773f4c9e4acbd42511aaa4e6620253b4bfc522ef
Polokego <pomakga022@student.wethinkcode.co.za>
2023-12-28 11:50:01 +0200

    tic-tac-toe: Initialised Game-Tree init states

--- tic-tac-toe_game-tree/html-deploy/script.js
+++ tic-tac-toe_game-tree/html-deploy/script.js
@@ -1,24 +1,32 @@
 let block = []
 
-var gameState = {
+let GameState = {
 
     prevState: null,
-    stateData: "",
-    lblData: null,
+    stateData: null,
     nextState: null
 
 }
-let gameTree = ["","","","","","","","","",""]
 
+let GameTree = [];
 //list of all winning game states
 const winStates = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8), (0,4,8),(2,4,6)]
 
 //functions for the buttons
 for (let k=0; k < 9; k++) {
 
+
+    GameTree.append(...GameState);
+    GameTree[k].stateData =  Document.getElementById("block" + (k+1));
+
     block.append(()=>{
 
+        let hadBotCircled = GameTree[k].stateData.textContent === 'O';
 
+        if(!hadBotCircled){
+
+            GameTree[k].stateData.textContent = "X";
+        }
 
 
     });
