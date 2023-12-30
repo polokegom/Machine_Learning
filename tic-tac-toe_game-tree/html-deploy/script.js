21b559f1a3b7d601aee595f62259461c4da085d4
Polokego <pomakga022@student.wethinkcode.co.za>
2023-12-30 15:34:08 +0200

    Game-Tree Skeleton

--- tic-tac-toe_game-tree/html-deploy/script.js
+++ tic-tac-toe_game-tree/html-deploy/script.js
@@ -1,44 +1,119 @@
 let blockClick = []
 
-let GameState = {
+let BlockState = {
 
     prevState: null,
+    indexOfState: 0,
     stateData: null,
+    instanceData: "",
     nextState: null
 
 }
 
-let GameTree = [];
-//list of all winning game states
-const winStates = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8), (0,4,8),(2,4,6)]
+let MainGameTree = [];
 
-//functions for the buttons
+
+//list of all winning game states
+const winStates = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8], [0,4,8],[2,4,6]]
+
+function popBlockType(instanceGameTree, char){
+
+    for (let k = 0; k < instanceGameTree.length; k++)
+        if (instanceGameTree[k] ===char) 
+            instanceGameTree.splice(k,1);
+    
+}
+
+
+function checkListMatch(list1, list2){
+    return list1.every(element => list2.includes(element));
+
+}
+
+alert(checkListMatch([0,2],[2,64,74,2,4,1,0,4,6,7]))
+
+
+
+//Onclick events for blocks
 for (let k=0; k < 9; k++) {
 
-
-    GameTree.push(JSON.parse(JSON.stringify(GameState)));
-    GameTree[k].stateData =  document.getElementById("block" + (k+ 1));    
+    let newBlockState = JSON.parse(JSON.stringify(BlockState));
+    newBlockState.indexOfState = k;
+    MainGameTree.push(newBlockState);
+    MainGameTree[k].stateData =  document.getElementById("block" + (k+ 1));    
     blockClick.push(()=>{
 
 
-        let hasBotCircled = GameTree[k].stateData.textContent === "O";
-        let isBlockEmpty = GameTree[k].stateData.textContent === ''; 
+        let hasBotCircled = MainGameTree[k].stateData.textContent === "O";
+        let isBlockEmpty = MainGameTree[k].stateData.textContent === ''; 
 
         if(!hasBotCircled && isBlockEmpty){
             //alert("number: " + k);
-           // alert(GameTree[k].stateData.textContent);
+           // alert(MainGameTree[k].stateData.textContent);
         
 
-            GameTree[k].stateData.textContent = "X";
+            MainGameTree[k].stateData.textContent = "X";
+            MainGameTree[k].stateData.style.color = "rgb(138, 21, 21)";
+
+            const tempRoot = Json.parse(Json.stringify(MainGameTree));
+            popBlockType(tempRoot,"X");
+            /*
+
+            findVictoryPath = (root,historyMoves,score, winningRoute)=> {
+                
+                
+                //find winning solution                
+                const chosen_X_Blocks = Json.parse(Json.stringify(root));
+
+                popBlockType(chosen_X_Blocks,"");
+                for(let p=0;p< chosen_X_Blocks.length; p++)
+                historyMoves.push(chosen_X_Blocks.indexOfState);
+
+                
+
+            }*/
+
         }
 
     });
 }
 
+const randBlockClick = Math.round(Math.random()*8);
+
+MainGameTree[randBlockClick].stateData.textContent = 'O';
+MainGameTree[randBlockClick].isntanceData = "O";
+MainGameTree[randBlockClick].stateData.style.color = "blue";
+
+
+
+
+function findVictoryPath(root,historicalMoves,score, winningRoute) {
+                
+                
+    //find winning solution                
+    const chosen_X_Blocks = Json.parse(Json.stringify(root));
+
+    popBlockType(chosen_X_Blocks,"");
+    for(let p=0;p< chosen_X_Blocks.length; p++)
+        historicalMoves.push(chosen_X_Blocks.indexOfState);
+
+    const isWinner = checkIfWinner(historicalMoves);
+    alert(isWinner);
+    
+
+}
+
+
+
+
 function restartGame() {
 
+    for (let k=0; k< 9;k++) 
+        GameTree[k].stateData.textContent = "";
+    const randBlockClick = Math.round(Math.random()*8);
 
-    blockClick = []
+    MainGameTree[randBlockClick].stateData.textContent = 'O';
+    MainGameTree[randBlockClick].stateData.style.color = "blue";
 
 
 }
