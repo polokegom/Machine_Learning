8ff9dcc563606baff6d2f98cff3696809ab24f03
Polokego <pomakga022@student.wethinkcode.co.za>
2024-01-13 22:19:26 +0200

    Done with Game-Tree project | Added Flocking AI simulator project

--- tic-tac-toe_game-tree/html-deploy/script.js
+++ tic-tac-toe_game-tree/html-deploy/script.js
@@ -1,6 +1,12 @@
-let blockClick = []
 
-//Define data-structure for storing block information
+
+
+let blockClick = []
+let MainGameTree = [];
+const maxWinReq  = 3
+let count = 0;
+
+//Define Object for storing Game block data
 let BlockState = {
 
     prevState: null,
@@ -11,55 +17,53 @@ let BlockState = {
 
 }
 
-let MainGameTree = [];
-
-
-//list of all winning game states
-const winStates = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8], [0,4,8],[2,4,6]];
-
-
-
 
 function popBlockType(instanceGameTree, char){
+    
     temp = []
-
     for (let k = 0; k < instanceGameTree.length; k++)
         if (instanceGameTree[k].stateData !==char) 
             temp.push(instanceGameTree[k]);
 
-   
-
     return  instanceGameTree.filter(gameState => gameState.stateData !==char)//temp;
-
     
 }
 
 
-
+/**
+ * OnClick event to reset the Game
+ */
 function restartGame() {
 
     for (let k=0; k< 9;k++) 
         GameTree[k].webElementParagraph.textContent = "";
     const randBlockClick = Math.round(Math.random()*8);
-
     MainGameTree[randBlockClick].webElementParagraph.textContent = 'O';
     MainGameTree[randBlockClick].webElementParagraph.style.color = "blue";
 
-
-}
-/**/
-function checkIsWinner(list1, list2){
-    return list1.every(element => list2.includes(element));
-
 }
 
 
+/**
+ * 0
+ * @param {list of indexes of played moves by agent} list1 
+ * @returns Whether player has won the game
+ */
+function checkIsWinner(list1){
+    
+    //list of all winning game states
+    const winStates = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8], [0,4,8],[2,4,6]];
+    return winStates.some(winCombo => {
+        return winCombo.every(chosenBlock =>list1.includes(chosenBlock));
+    });
+
+}
 
 
 //Onclick events for blocks
-// ------------------------------------------------------------
-///
 for (let k=0; k < 9; k++) {
+
+
 
     let newBlockState = JSON.parse(JSON.stringify(BlockState));
     newBlockState.indexOfState = k;
@@ -67,7 +71,6 @@ for (let k=0; k < 9; k++) {
     MainGameTree[k].webElementParagraph =  document.getElementById("block" + (k+ 1));    
 
     //Onclick events for blocks
-    //-------------------------------------------------------------
     blockClick.push(()=>{
 
 
@@ -75,50 +78,30 @@ for (let k=0; k < 9; k++) {
         let isBlockEmpty = MainGameTree[k].webElementParagraph.textContent === ''; 
 
         if(!hasBotCircled && isBlockEmpty){
-           // alert(MainGameTree[k].webElementParagraph.textContent);
         
             MainGameTree[k].webElementParagraph.textContent = "X";
             MainGameTree[k].stateData = "X";
             MainGameTree[k].webElementParagraph.style.color = "rgb(138, 21, 21)";
-            //alert((MainGameTree));
+            //A.I. bot moves
+            let tempRoot = JSON.parse(JSON.stringify(MainGameTree));       
+            count =0;
+            winningAIRoutes = []
+            winningPlayerRoutes = []
 
-            const tempRoot = MainGameTree.filter(gameState  => gameState.stateData !== "X");//[].concat(MainGameTree);;
-     
-
-            //tempRoot f
-           //for (let q=0;q < tempRoot.length;q++)
-           //     alert(tempRoot[q].indexOfState);
-
-            findVictoryPath(tempRoot,[],null, null);
-
-
-            /*
-            findVictoryPath = (root,historyMoves,score, winningRoute)=> {
-                
-                
-                //find winning solution                
-                const chosen_X_Blocks = Json.parse(Json.stringify(root));
-
-                popBlockType(chosen_X_Blocks,"");
-                for(let p=0;p< chosen_X_Blocks.length; p++)
-                historyMoves.push(chosen_X_Blocks.indexOfState);
-
-                
-
-            }*/
+            findVictoryPath(tempRoot,[], winningAIRoutes,[], winningPlayerRoutes,false);
+            //Analyse dataset & make move
+            playerHighWinRate = winningPlayerRoutes.filter(winRoute => winRoute.length <= Math.floor(maxWinReq/2))
+            
+            alert("All possible ways A.I. to wins: " + winningAIRoutes.length)
+            alert("All possible ways Player to wins: " + winningPlayerRoutes.length)
+            alert("All definite Player win states:" + playerHighWinRate.length)
 
         }
 
     });
-    //-------------------------------------------------------------
+    
 
 }
-
-
-//
-//  -------------------------------------------------------------
-//
-
 
 
 function main() {
@@ -130,42 +113,85 @@ function main() {
 
 }
 
-main();
 
-alert([3,4,1,8,83,244,5,33,2].filter(num => num !==0));
+main()
 
-
-function findVictoryPath(root,historicalMoves,score, winningRoute) {
+function findVictoryPath(root,historicalAIMoves,winningAIRoute, historicalPlayerMoves, winningPlayerRoute, isEnemiesTurn) {
                 
+    //Checks number of instance the game tree is running
+    count += 1;
+    console.log("Instances running: " + count)
 
-    //find winning solution                
-    // ---------------------------------------------------
-   // let chosen_X_Blocks = [].concat(root);
-    let chosen_O_Blocks = root.filter(gameState => gameState.stateData !== "" && gameState.stateData!== "X")//popBlockType(chosen_X_Blocks,"");
-    let nextChosen_O_Block = root.filter(gameState => gameState.stateData !== "X" && gameState.stateData !== "O");
-  
-    //for(let p=0;p< chosen_O_Blocks.length; p++)
-    historicalMoves.push(chosen_O_Blocks[0].indexOfState);
-    alert(historicalMoves);
-
-    let isWinner = checkIsWinner(historicalMoves,winStates);
+    let playerMoves = []
+    let aiMoves = []
     
-    if(isWinner){
-        alert("you have won");
-        findVictoryPath(nextChosen_0_Block, historicalMoves, null, null)
+    //Get all moves Player & AI made using Game State
+    root.forEach((move)=> {
 
-    }else {
+        if (move.stateData == "X")
+            playerMoves.push(move.indexOfState);
+        if (move.stateData == "O")
+            aiMoves.push(move.indexOfState);
+    });
 
-        if (nextChosen_O_Block.length > 0){
-            nextChosen_0_Block[0].stateData  = "X";
-            findVictoryPath(nextChosen_0_Block, historicalMoves, null, null)
+ 
+    let isPlayerWinner = false;
+    let isAIWinner = false;
+    
+    //Indexes of empty blocks
+    let emptyBlockIndexes = [];
+    for (let r =0; r < root.length; r++)
+        if (root[r].stateData == "")
+            emptyBlockIndexes.push(root[r].indexOfState);
+
+            
+    //Check if Player or A.I. has won
+    if (!isEnemiesTurn){
+        isAIWinner = checkIsWinner(aiMoves);
+        
+    } else{
+        isPlayerWinner = checkIsWinner(playerMoves);
+
+    }
+
+    
+    if(isAIWinner){   
+
+
+        //alert("you have won");
+        winningAIRoute.push(historicalAIMoves)
+        //findVictoryPath(nextChosen_Block, historicalAIMoves,historicalPlayerMoves, null, !isEnemiesTurn)
+
+    }else if (isPlayerWinner) {
+
+        winningPlayerRoute.push(historicalPlayerMoves)
+    } else {
+
+        //Create new instance
+        emptyBlockIndexes.forEach((emptyBlockIndex)=>{
+            let newGameTree = JSON.parse(JSON.stringify(root));
+            let pastAIMoves =  JSON.parse(JSON.stringify(historicalAIMoves));
+            let pastPlayerMoves =  JSON.parse(JSON.stringify(historicalPlayerMoves));
+
+            if (!isEnemiesTurn){
+                    
+                newGameTree[emptyBlockIndex].stateData  = "O"; 
+                pastAIMoves.push(emptyBlockIndex);                    
+            } else {
+
+                newGameTree[emptyBlockIndex].stateData  = "X";
+                pastPlayerMoves.push(emptyBlockIndex); 
+            }                
+            findVictoryPath(newGameTree, pastAIMoves, winningAIRoutes, pastPlayerMoves, winningPlayerRoute, !isEnemiesTurn);
+
+        });
 
         }
     }
     // -----------------------------------------------------
 
 
-}
+
 
 
 
