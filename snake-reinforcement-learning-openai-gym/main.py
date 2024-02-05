b70a890a43edf088e524c679308024468d5f28c8
Polokego Makgakge <145930950+polokegom@users.noreply.github.com>
2024-02-05 05:05:25 +0200

    Update main.py

--- snake-reinforcement-learning-openai-gym/main.py
+++ snake-reinforcement-learning-openai-gym/main.py
@@ -1,7 +1,7 @@
 import gym_snake_game
 import gym
 
-"Setup environment for Reinforcement (RL) Learning"
+"Setup environment for Reinforcement Learnin (RL)"
 env = gym.make('Snake-v0', render_mode='human')
 env = gym_snake_game.make('Snake-v0', render_mode='human')
 env.reset()
